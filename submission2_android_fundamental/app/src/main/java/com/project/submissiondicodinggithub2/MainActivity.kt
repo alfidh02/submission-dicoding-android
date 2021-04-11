2fb32227ab03ea9d32eb00679622030e27f43250
alfidh02 <alfidarmawan79@gmail.com>
2021-04-11 18:58:01 +0700

    :rocket: add submission 2 file (not yet finished)

---
+++ submission2_android_fundamental/app/src/main/java/com/project/submissiondicodinggithub2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.project.submissiondicodinggithub2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
