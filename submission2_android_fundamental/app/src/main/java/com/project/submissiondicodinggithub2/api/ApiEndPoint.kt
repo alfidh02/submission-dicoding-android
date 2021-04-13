8b889c43232fa9254baf23d6558b1f7e2992df8e
alfidh02 <alfidarmawan79@gmail.com>
2021-04-14 00:29:40 +0700

    :rocket: add final token revision for submission 2 (finish)

--- submission2_android_fundamental/app/src/main/java/com/project/submissiondicodinggithub2/api/ApiEndPoint.kt
+++ submission2_android_fundamental/app/src/main/java/com/project/submissiondicodinggithub2/api/ApiEndPoint.kt
@@ -12,26 +12,26 @@ import retrofit2.http.Query
 
 interface ApiEndPoint {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getSearchUser(@Query("q") query: String): Call<UserArray>
 
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getDetailUser(
         @Path("username") username: String
     ): Call<UserDetail>
 
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getFollowersUser(
         @Path("username") username: String
     ): Call<ArrayList<User>>
 
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getFollowingUser(
         @Path("username") username: String
     ): Call<ArrayList<User>>
